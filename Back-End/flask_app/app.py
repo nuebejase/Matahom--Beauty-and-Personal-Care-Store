00c1421c14f27993fadfc40017b25b56ab9882cd
nuebejase <jasekarlzerrudo@gmail.com>
2025-12-08 21:31:33 +0800

    minor changes

--- Back-End/flask_app/app.py
+++ Back-End/flask_app/app.py
@@ -7,6 +7,8 @@ from routes.product_routes import product_routes
 from routes.order_routes import order_routes
 from routes.category_routes import category_routes
 from routes.order_items_routes import order_items_routes
+from routes.inventory_routes import inventory_routes
+from routes.payment_routes import payment_routes
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,6 +21,8 @@ app.register_blueprint(product_routes)
 app.register_blueprint(order_routes)
 app.register_blueprint(category_routes)
 app.register_blueprint(order_items_routes)
+app.register_blueprint(inventory_routes)
+app.register_blueprint(payment_routes)
 
 @app.route("/")
 def index():
